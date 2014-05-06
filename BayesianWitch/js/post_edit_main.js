e78366c6b5355c97e3d3073d5db01a9a203eeefd
Chris Stucchio <stucchio@gmail.com>
2014-05-06 17:25:00 +0530

    Before alternate titles are displayed, show a loading indicator.

--- BayesianWitch/js/post_edit_main.js
+++ BayesianWitch/js/post_edit_main.js
@@ -1,35 +1,59 @@
-bw_add_box_counter = 0;
+(function() {
+    var loadingMoreIndicatorId = "bayesian_witch_loading_more";
+    var displayedAllTitles = false;
+    setTimeout(function() {
+        if (!displayedAllTitles) {
+            try {
+                var loadingMoreIndicator = document.createElement("p");
+                loadingMoreIndicator.innerHTML = "Please wait - checking for alternative titles..."
+                loadingMoreIndicator.id = loadingMoreIndicatorId;
+                document.getElementById("titlediv").appendChild(loadingMoreIndicator);
+            } catch (err) {
+            }
+        }
+    }, 10);
 
-jQuery(window).ready(function(){
-  //bandit title
-  jQuery(document).on('click', '.bw-admin-title-delete', function(){
-    jQuery(this).parent().remove();
-  });
-  jQuery(document).on('click', '.bw-admin-title-add', function(){
-    bw_generate_title_box();
-  });
+    bw_add_box_counter = 0;
 
-  bw_generate_add_button();
-});
+    var hideLoadingIndicator = function() {
+        displayedAllTitles = true;
+        jQuery("#" + loadingMoreIndicatorId).remove(); //remove loading more indicator
+    };
 
-function bw_generate_title_box(title, tag){
-  title = title || '';
-  tag = tag || get_random_tag();
-  var html =
-    '<div>' +
-    '<input class="bw-admin-title" type="text" name="bw-titles['+tag+']" size="30" value="'+title+'" autocomplete="off">' +
-    '<input type="button" class="bw-admin-title-delete button" value="Delete">' +
-    '</div>';
-  jQuery('#bw-add-button-wrap').before(html);
-}
+    window.bw_generate_title_box = function(title, tag){
+        hideLoadingIndicator();
+
+        title = title || '';
+        tag = tag || get_random_tag();
+        var html =
+            '<div>' +
+            '<input class="bw-admin-title" type="text" name="bw-titles['+tag+']" size="30" value="'+title+'" autocomplete="off">' +
+            '<input type="button" class="bw-admin-title-delete button" value="Delete">' +
+            '</div>';
+        jQuery('#bw-add-button-wrap').before(html);
+    };
 
 
-function bw_generate_add_button(){
-  var html = '<div id="bw-add-button-wrap"><input type="button" class="bw-admin-title-add button" value="Add title variation">' +
-    '<div class="clear"></div></div>';
-  jQuery('#titlewrap').append(html);
-}
+    var bw_generate_add_button = function(){
+        var html = '<div id="bw-add-button-wrap"><input type="button" class="bw-admin-title-add button" value="Add title variation">' +
+            '<div class="clear"></div></div>';
+        jQuery('#titlewrap').append(html);
+        hideLoadingIndicator();
+    };
 
-function get_random_tag(){
-  return 'TitleVariation_'+'0000000000'.split('').map(function(){return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.charAt(Math.floor(62*Math.random()));}).join('');
-}
+    var get_random_tag = function(){
+        return 'TitleVariation_'+'0000000000'.split('').map(function(){return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.charAt(Math.floor(62*Math.random()));}).join('');
+    };
+
+    jQuery(window).ready(function(){
+        //bandit title
+        jQuery(document).on('click', '.bw-admin-title-delete', function(){
+            jQuery(this).parent().remove();
+        });
+        jQuery(document).on('click', '.bw-admin-title-add', function(){
+            window.bw_generate_title_box();
+        });
+
+        bw_generate_add_button();
+    });
+})()
